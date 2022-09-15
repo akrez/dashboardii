946d9ae87884b592dbf37d31004b8f005360c204
Akrez <akrez.like@gmail.com>
2022-09-16 02:33:45 +0430

    feat:based on AdminLteCustom

--- views/layouts/adminlte.php
+++ views/layouts/adminlte.php
@@ -4,21 +4,65 @@
 /** @var string $content */
 
 use app\assets\AdminLteAsset;
-use app\components\AdminLte;
+use app\components\AdminLteCustom;
 use app\widgets\Alert;
 use yii\helpers\Html;
+use yii\web\View;
 
 AdminLteAsset::register($this);
 //
-$this->registerJs('
-');
+$this->registerJs("
+$(document).on('click','.btn[toggle]',function() {
+
+    var btn = $(this);
+    var isHidden = $(btn.attr('toggle')).is(':hidden');    
+
+    $('.btn[toggle]').each(function(i) {
+        var toggleBtn = $(this);
+        $(toggleBtn.attr('toggle')).hide();
+        toggleBtn.addClass('btn-default');
+        toggleBtn.removeClass('btn-warning');
+    });
+
+    if(isHidden) {
+        $(btn.attr('toggle')).show();
+        btn.addClass('btn-warning');
+        btn.removeClass('btn-default');
+    }
+
+});
+$(document).on('pjax:beforeSend', function(xhr, options) {
+    $('.ajax-splash-show').css('display','inline-block');
+    $('.ajax-splash-hide').css('display','none');
+});
+$(document).on('pjax:complete', function(xhr, textStatus, options) {
+    $('.ajax-splash-show').css('display','none');
+    $('.ajax-splash-hide').css('display','inline-block');
+});
+", View::POS_READY);
 $this->registerCss("
 .splash-style {
-    background-image: url('" . Yii::getAlias('@web/cdn/img/loading.svg') . "');
+    background-image: url('" . Yii::getAlias('@web/img/loading.svg') . "');
+    display: none;
+    background-color: rgba(0, 0, 0, 0.67);
+    inset: 0px;
+    position: absolute;
+    z-index: 9998;
+    background-repeat: no-repeat;
+    background-position: center;
 }
 .sidebar-mini.sidebar-collapse .user-panel {
     display: none !important;
     -webkit-transform: translateZ(0);
+}
+.table th {
+    vertical-align: top !important;
+}
+.table td {
+    vertical-align: middle !important;
+}
+.position-relative {
+    .position: relative;
 }
 ");
 //
@@ -38,13 +82,13 @@ $this->title = Html::encode($this->title ? $this->title : Yii::$app->name);
     <?php $this->head() ?>
 </head>
 
-<body class="skin-blue sidebar-mini <?= AdminLte::isCollapse() ? 'sidebar-collapse' : '' ?>">
+<body class="skin-blue sidebar-mini <?= AdminLteCustom::isCollapse() ? 'sidebar-collapse' : '' ?>">
     <?php $this->beginBody() ?>
     <div class="wrapper">
         <!-- Main Header -->
         <header class="main-header">
             <!-- Logo -->
-            <?php echo AdminLte::renderTitle(AdminLte::getTitleLink(), AdminLte::getTitleText()); ?>
+            <?php echo AdminLteCustom::renderTitle(); ?>
             <!-- Header Navbar -->
             <nav class="navbar navbar-static-top" role="navigation">
                 <!-- Sidebar toggle button-->
@@ -54,9 +98,9 @@ $this->title = Html::encode($this->title ? $this->title : Yii::$app->name);
                 <!-- Navbar Right Menu -->
                 <div class="navbar-custom-menu">
                     <ul class="nav navbar-nav">
-                        <?= AdminLte::renderNavList(AdminLte::getNavList()) ?>
+                        <?= AdminLteCustom::renderNavList() ?>
                     </ul>
-                    <?= Html::beginForm(['/user/signout'],'POST',["id"=>"logout-form","style"=>"display: none;",]).Html::endForm() ?>
+                    <?= Html::beginForm(['/users/signout'], 'POST', ["id" => "logout-form", "style" => "display: none;",]) . Html::endForm() ?>
                 </div>
             </nav>
         </header>
@@ -64,18 +108,14 @@ $this->title = Html::encode($this->title ? $this->title : Yii::$app->name);
         <aside class="main-sidebar">
             <aside class="main-sidebar direction">
                 <section class="sidebar">
-                    <div class="user-panel">
-                        <?php echo AdminLte::renderLogo(AdminLte::getLogo()); ?>
-                    </div>
-                    <ul class="sidebar-menu">
-                        <?php echo AdminLte::renderMenuTitle(AdminLte::getMenuTitle()); ?>
-                        <?php echo AdminLte::renderMenuList(AdminLte::getMenuList()); ?>
-                    </ul>
+                    <?php echo AdminLteCustom::renderLogo(); ?>
+                    <?php echo AdminLteCustom::renderMenu(); ?>
                 </section>
             </aside>
         </aside>
         <!-- Content Wrapper. Contains page content -->
-        <div class="content-wrapper">
+        <div class="content-wrapper position-relative">
+            <div class="ajax-splash-show splash-style"></div>
             <!-- Content Header (Page header) -->
             <div class="col-xs-12">
                 <br />

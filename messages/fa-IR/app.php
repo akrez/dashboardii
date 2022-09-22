2604ab808d6d1fa02bdce2e914c06bc133b30355
Akrez <akrez.like@gmail.com>
2022-09-22 15:32:06 +0330

    feat:add translates

--- messages/fa-IR/app.php
+++ messages/fa-IR/app.php
@@ -8,8 +8,12 @@ return [
     'If the captcha is illegible, click on it.' => 'اگر کد تأیید ناخوانا است، روی آن کلیک کنید.',
     //
     'Menus' => 'منوها',
+    'Menu Content' => 'محتوای منو',
+    'Menu Chart' => 'چارت منو',
+    'Menu Visits' => 'بازدیدهای منو',
     //
     'Create' => 'ایجاد',
     'Update' => 'ویرایش',
     'Edit' => 'ویرایش کنید',
+    'Import' => 'ذخیره',
 ];
